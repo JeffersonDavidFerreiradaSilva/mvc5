65b3d9c19889ab16ac0cf10842a3db2d631c5da2
Jefferson David <napsterdavid@gmail.com>
2023-08-26 11:48:31 -0300

    conexao com o banco de dados e criando a tabela se for nescessário

--- model/model_aluno.py
+++ model/model_aluno.py
@@ -3,6 +3,20 @@ import sqlite3
 class aluno_model():
     
     def __init__(self):
-        self.conexao = sqlite3.connect(
-            
-        )
+        self.conexao = sqlite3.connect("SERPRO")
+        self.cursor = self.conexao.cursor()
+        
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS tb_alunos"
+                            '('
+                            'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                            'nome TEXT NOT NULL,'
+                            'idade INTEGER NOT NULL,'
+                            'pese REAL NOT NULL'
+                            ' )'
+            )
+        self.conexao.commit()
+        self.cursor.close()
+        
+    
+    
+    
